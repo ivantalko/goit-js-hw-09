020090545f96120910e18e77de29f2c50a1706fa
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-09-16 17:53:15 +0300

    jason

---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
