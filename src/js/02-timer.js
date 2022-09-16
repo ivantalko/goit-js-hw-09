9af84b77e32ee9f9971cc96895fb959da68b0f0d
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-09-16 19:20:38 +0300

    home work

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,16 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+const input = document.querySelector('#datetime-picker');
+const buttonStart = document.querySelector(button[data - start]);
+const dayTimer = document.querySelector([data - days]);
+const hoursTimer = document.querySelector([data - hours]);
+const minutesTimer = document.querySelector([data - minutes]);
+const secondsTimer = document.querySelector([data - second]);
+buttonStart.disabled = true;
+
+
+buttonStart.addEventListener
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -12,3 +22,25 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+flatpickr('#datetime-picker', options);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value); {
+padStart(2, '0')
+}
