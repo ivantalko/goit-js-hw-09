97bcbff3d4bcf0220f0dfec10f49eb826f56f9be
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-09-20 09:34:14 +0300

    import

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+import { convertMs } from '../js/convertHelp';
 
 const input = document.querySelector('#datetime-picker');
 const buttonStart = document.querySelector('[data-start]');
@@ -61,21 +62,3 @@ const options = {
   },
 };
 flatpickr(input, options);
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
